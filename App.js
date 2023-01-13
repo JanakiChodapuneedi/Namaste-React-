7e3743a5d3d37d6c9e856b964fae6fc67f44e509
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-01-13 17:28:50 +0530

    chapter4

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 // Manipulate the HTML DOM using Javscript
 // const heading = document.createElement("h1");
 // heading.innerHTML = "Namaste Everyone";
@@ -9,30 +9,452 @@ import  ReactDOM from "react-dom";
 // Manipulate the HTML DOM using React
 
 // Create nested React Elements
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
+// const heading = React.createElement(
+//     "h1",
+//     {
+//       id: "title",
+//     },
+//     "heading"
+//   );
+//   const heading1 = React.createElement(
+//     "h1",
+//     {
+//       id: "title",
+//     },
+//     "heading1"
+//   );
+
+//   const container = React.createElement(
+//     "div",
+//     {
+//       id: "container",
+//     },
+//     [heading, heading1]
+//   );
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        alt="logo"
+        className="logo"
+        src="https://tse1.mm.bing.net/th?id=OIP.5g7g-Qnr_bnxkbw_eQaoMwHaEp&pid=Api"
+      />
+    </a>
   );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
+};
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
-  
-  const container = React.createElement(
-    "div",
+};
+const restaurantlist = [
+  {
+    type: "restaurant",
+    data: 
     {
-      id: "container",
+      type: "F",
+      id: "458207",
+      name: "KFC",
+      uuid: "1d2db7d1-bba4-46ed-bc6e-8501f5850eec",
+      city: "10401",
+      area: "Tanuku",
+      totalRatingsString: "100+ ratings",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      tags: [],
+      costForTwo: 40000,
+      costForTwoString: "₹400 FOR TWO",
+      deliveryTime: 21,
+      minDeliveryTime: 21,
+      maxDeliveryTime: 21,
+      slaString: "21 MINS",
+      lastMileTravel: 1.2000000476837158,
+      slugs: {
+        restaurant: "kfc-tanuku-sajjapuram",
+        city: "tanuku",
+      },
+      cityState: "10401",
+      address:
+        "D.No.25-1-9 in R.S.No.63 1, Ward No.20, Velpur Road, Tanuku, Tanuku Municipality, West Godavari District, Andhra Pradesh  534211",
+      locality: "Velpur Road",
+      parentId: 547,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "40% off",
+        shortDescriptionList: [
+          {
+            meta: "40% off | Use SWIGGYIT",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "40% off up to ₹80 | Use code SWIGGYIT",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "40% OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use SWIGGYIT",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "40% off up to ₹80 | Use code SWIGGYIT",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      ribbon: [{ type: "PROMOTED" }],
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=5715677~p=1~eid=00000185-aab6-be32-31eb-f8760031015e",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "1.2 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "458207",
+        deliveryTime: 21,
+        minDeliveryTime: 21,
+        maxDeliveryTime: 21,
+        lastMileTravel: 1.2000000476837158,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.0",
+      totalRatings: 100,
+      new: false,
     },
-    [heading, heading1]
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "544645",
+      name: "Madhuram's Sweets & Cakes",
+      uuid: "daf8d299-1319-44b0-90f3-cbe1df351e8b",
+      city: "10401",
+      area: "R P Road",
+      totalRatingsString: "50+ ratings",
+      cloudinaryImageId: "brbtodqprkjchc0fwomp",
+      cuisines: ["Sweets", "Snacks", "Bakery"],
+      tags: [],
+      costForTwo: 20000,
+      costForTwoString: "₹200 FOR TWO",
+      deliveryTime: 16,
+      minDeliveryTime: 16,
+      maxDeliveryTime: 16,
+      slaString: "16 MINS",
+      lastMileTravel: 0.30000001192092896,
+      slugs: {
+        restaurant: "madhuram's-sweets-&-cakes-sajjapuram-sajjapuram",
+        city: "tanuku",
+      },
+      cityState: "10401",
+      address:
+        "D.No 35-1-4/1,Municipality Area ,Tanuku,East Godavari,AP,534211",
+      locality: "Sajjapuram",
+      parentId: 327201,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "50% off",
+        shortDescriptionList: [
+          {
+            meta: "50% off | Use WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹80 | Use code WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "50% OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹80 | Use code WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "0.3 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "544645",
+        deliveryTime: 16,
+        minDeliveryTime: 16,
+        maxDeliveryTime: 16,
+        lastMileTravel: 0.30000001192092896,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.0",
+      totalRatings: 50,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "645454",
+      name: "Hotel Sai Krishna",
+      uuid: "c01a4b26-b2f2-4d8d-acbd-26d35acff018",
+      city: "10401",
+      area: "R.P. Road",
+      totalRatingsString: "Too Few Ratings",
+      cloudinaryImageId: "f1bc9ddf53de574cdc35ab2f717df234",
+      cuisines: ["South Indian"],
+      tags: [],
+      costForTwo: 15000,
+      costForTwoString: "₹150 FOR TWO",
+      deliveryTime: 17,
+      minDeliveryTime: 17,
+      maxDeliveryTime: 17,
+      slaString: "17 MINS",
+      lastMileTravel: 0.800000011920929,
+      slugs: {
+        restaurant: "hotel-sai-krishna-sajjapuram-sajjapuram",
+        city: "tanuku",
+      },
+      cityState: "10401",
+      address:
+        "20-1-51, opp Government Area Hospital, R.P. Road, Tanuku, West Godavari, Andhra Pradesh - 534211",
+      locality: "R.P. Road",
+      parentId: 101790,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "50% off",
+        shortDescriptionList: [
+          {
+            meta: "50% off | Use WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹80 | Use code WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "50% OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹80 | Use code WELCOME50",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "0.8 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "645454",
+        deliveryTime: 17,
+        minDeliveryTime: 17,
+        maxDeliveryTime: 17,
+        lastMileTravel: 0.800000011920929,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "--",
+      totalRatings: 0,
+      new: true,
+    },
+    subtype: "basic",
+  },
+];
+
+const RestaurantCard = ({name,cuisines,deliveryTime,cloudinaryImageId}) => {
+  return (
+    <div className="card">
+      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines }</h3>
+      <h4>{deliveryTime}</h4>
+    </div>
   );
-  
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+};
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {restaurantlist.map((restaurant) => {
+        return <RestaurantCard { ...restaurant.data } key={restaurant.data.id} />;
+      })}
+    </div>
+  );
+};
+const Footer = () => {
+  return <div> Footer </div>;
+};
+const Applayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(<Applayout />);
