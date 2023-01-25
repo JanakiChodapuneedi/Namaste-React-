4d22c40ca61675bc32a8d466e87d10eaf507f903
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-01-25 16:53:48 +0530

    exploring the world:5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
-import { restaurantlist } from "../constants";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+
+  useEffect(()=>{
+    getRestaurants();
+  },[]);
+  async function getRestaurants(){
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8996676&lng=77.4826837&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+  
     const [searchTxt,setSearchText]=useState("");
-    const [restaurants,setRestaurants]=useState(restaurantlist);
+    const [allRestaurants,setAllRestaurants]=useState([]);
+    const [filteredRestaurants,setFilteredRestaurants] =useState([]);
     const searchFilter = (searchTxt,restaurants) =>{
-      const filteredData=restaurants.filter((restaurant)=>restaurant.data.name.includes(searchTxt)
-       )
+      const filteredData = allRestaurants.filter((restaurant)=>restaurant.data.name.toLowerCase().includes(searchTxt.toLowerCase()));
        return filteredData;
     };
-    return (
+   
+    
+    return allRestaurants?.length===0 ? <Shimmer/> : (
     <>
     <div className="search-container">
     <input type="text"
@@ -21,15 +34,17 @@ const Body = () => {
     />
     <button className="search-btn"
     onClick={()=>{
-      const data = searchFilter(searchTxt,restaurants);
-    setRestaurants(data)}}
+      const data = searchFilter(searchTxt,filteredRestaurants);
+    setFilteredRestaurants(data)}}
     >Search</button>
     </div>
+   { (filteredRestaurants?.length===0)? <h1> No restaurants matched with your search</h1>: 
+
     <div className="restaurant-list">
-      {restaurants.map((restaurant) => {
+      {filteredRestaurants.map((restaurant) => {
         return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
       })}
-    </div>
+    </div> }
     </>
     
   );
---
+++ src/components/Shimmer.js
@@ -0,0 +1,5 @@
+const Shimmer = () => {
+    return <h1>My page is loading with shimmer effect</h1>
+};
+
+export default Shimmer;
