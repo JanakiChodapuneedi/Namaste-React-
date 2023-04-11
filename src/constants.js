df5110550d6147fc42457b797f2cb5d8f4114e74
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-04-11 16:21:22 +0530

    cors url change

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,9 @@ export const SWIGGY_IMG_CDN_URL =
     "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const RESTAURANT_MENU_CDN_URL = 
     //"https://www.swiggy.com/dapi/menu/v4/full?lat=12.8996676&lng=77.4826837&menuId=";
-    "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8996676&lng=77.4826837&restaurantId=";
+    //"https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8996676&lng=77.4826837&restaurantId=";
+    "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8996676&lng=77.4826837&restaurantId=";
 
 export const ALL_RESTAURANT_DATA_CDN_URL = 
-     "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8996676&lng=77.4826837&page_type=DESKTOP_WEB_LISTING";
+    // "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8996676&lng=77.4826837&page_type=DESKTOP_WEB_LISTING";
+    "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8996676&lng=77.4826837&page_type=DESKTOP_WEB_LISTING";
